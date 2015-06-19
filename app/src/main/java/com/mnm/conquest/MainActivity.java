5ee5dcf91a7fed29f08be66d21660ed9bb4a4041
shomimn <shomimn@gmail.com>
2015-06-19 17:45:09 +0200

    toplel

--- app/src/main/java/com/mnm/conquest/MainActivity.java
+++ app/src/main/java/com/mnm/conquest/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity
 {
+    private int milos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
