ff9db741bdb46d0dfbd834b7eac02d6acf2d3233
shomimn <shomimn@gmail.com>
2015-06-20 21:11:53 +0200

    taskmanager

--- app/src/main/java/com/mnm/conquest/MainActivity.java
+++ app/src/main/java/com/mnm/conquest/MainActivity.java
@@ -1,7 +1,6 @@
 package com.mnm.conquest;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
@@ -12,6 +11,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener
@@ -22,13 +25,13 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button sign_up_button = (Button)findViewById(R.id.sign_up_login_button);
+        Button sign_up_button = (Button) findViewById(R.id.sign_up_login_button);
         sign_up_button.setOnClickListener(this);
 
-        Button sign_in_button = (Button)findViewById(R.id.sign_in_button);
+        Button sign_in_button = (Button) findViewById(R.id.sign_in_button);
         sign_in_button.setOnClickListener(this);
 
-        map_button = (Button)findViewById(R.id.map_button);
+        map_button = (Button) findViewById(R.id.map_button);
         map_button.setOnClickListener(this);
     }
 
