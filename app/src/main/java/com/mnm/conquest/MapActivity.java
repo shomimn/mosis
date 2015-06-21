79a7ac7f4e8e1556fec72f66fa032d07a334e375
NemanjaT1992 <nemanjatomasevic71@gmail.com>
2015-06-20 21:27:28 -0700

    circleView

--- app/src/main/java/com/mnm/conquest/MapActivity.java
+++ app/src/main/java/com/mnm/conquest/MapActivity.java
@@ -4,18 +4,15 @@ import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 
 
@@ -23,29 +20,31 @@ public class MapActivity extends AppCompatActivity
 {
 
     private GoogleMap map;
-    private static LatLng mylocation;
-    private Location location;
-    private LocationManager locationManager;
-    String provider;
-    Criteria cri;
+    private CircularView circularView;
+    private Context cont = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        cont = this;
         map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
         map.setMyLocationEnabled(true);
 
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        cri=new Criteria();
-        provider=locationManager.getBestProvider(cri, false);
-        location=locationManager.getLastKnownLocation(provider);
-        mylocation = new LatLng(location.getLatitude(), location.getLongitude());
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(mylocation, 15));
-
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
+        circularView = (CircularView) findViewById(R.id.circularView);
+        circularView.setVisibility(View.GONE);
+
+
+        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+
+                circularView.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     @Override
