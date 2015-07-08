be728f45d4fe2972c628818fa398c34d2b981ae7
NemanjaT1992 <nemanjatomasevic71@gmail.com>
2015-07-07 18:50:20 -0700

    aa

---
+++ app/src/main/java/com/mnm/conquest/HighScoreActivity.java
@@ -0,0 +1,182 @@
+package com.mnm.conquest;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.mnm.conquest.ecs.Component;
+import com.mnm.conquest.ecs.Game;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+public class HighScoreActivity extends ActionBarActivity
+{
+    private ListView listView;
+    public ArrayList<PlayerInfo> players;
+    public ArrayList<Score> playerStats;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_score);
+
+        players = new ArrayList<>();
+        listView = (ListView)findViewById(R.id.high_score_list);
+
+        getPlayers();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_high_score, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void getPlayers()
+    {
+        Task.Data task = new Task.Data(Game.getPlayerInfo().getUsername(), new Task.Data.DataReadyCallback()
+        {
+            @Override
+            public void dataReady()
+            {
+                JSONArray data =  getData();
+                try
+                {
+                    HighScoreActivity.this.players.clear();
+                    playerStats = new ArrayList<>();
+                    for(int i=0; i<data.length(); ++i)
+                    {
+                        JSONObject object = (JSONObject)data.get(i);
+                        int level = object.getInt("level");
+                        int kills = object.getInt("kills");
+                        int deaths = object.getInt("deaths");
+
+                        int score;
+                        if((kills - deaths) > 0)
+                            score = level*(kills - deaths);
+                        else score = 0;
+
+                        //playerStats.add(object.get("username").toString()+" "+String.valueOf(score));
+                        playerStats.add(new Score(score, object.getString("username")));
+                        HighScoreActivity.this.players.add(new PlayerInfo(object));
+                    }
+                    if(playerStats != null)
+                    {
+                        Collections.sort(playerStats, new Comparator<Score>(){
+                        public int compare(Score s1, Score s2) {
+                            //return s1.getScore().compareTo(s2.getScoreStr());
+                            return Integer.valueOf(s2.getScore()).compareTo(s1.getScore());
+                        }});
+                        HighScoreAdapter adapter = new HighScoreAdapter(HighScoreActivity.this, playerStats);
+                        listView.setAdapter(adapter);
+                    }
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        })
+        {
+            @Override
+            public void executeImpl()
+            {
+                ServerConnection.getAllPlayers();
+            }
+        };
+        TaskManager.getTaskManager().execute(task);
+    }
+    public class HighScoreAdapter extends ArrayAdapter<Score>
+    {
+        private final Context context;
+        private final List<Score> values;
+        public HighScoreAdapter(Context context, List<Score> values)
+        {
+            super(context, -1, values);
+            this.context = context;
+            this.values = values;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View rowView = inflater.inflate(R.layout.list_view_high_score, parent, false);
+            TextView username = (TextView)rowView.findViewById(R.id.high_score_username);
+            TextView highScore = (TextView)rowView.findViewById(R.id.high_score_value);
+            //String[] un = values.get(position).split(" ");
+            username.setText(values.get(position).getUsername());
+            highScore.setText(String.valueOf(values.get(position).getScore()));
+
+            //Collections.sort(players, new ScoreComparator());
+
+//            Collections.sort(playerStats, new Comparator<Score>()
+//            {
+//                @Override
+//                public int compare(Score lhs, Score rhs)
+//                {
+//                    return lhs.score > rhs.score ? lhs.score : rhs.score;
+//                }
+//            });
+
+            ImageView image = (ImageView)rowView.findViewById(R.id.high_score_photo);
+            image.setImageBitmap(players.get(position).getPhoto());
+            //image.setImageResource(players.get(position).getMarkerId());
+
+            return rowView;
+        }
+    }
+    private class Score
+    {
+        int score;
+        String username;
+
+        public Score(int i, String s)
+        {
+            score = i;
+            username = s;
+        }
+        public int getScore(){return score;}
+        public String getUsername() {return username;}
+        public String getScoreStr() {return String.valueOf(score);}
+    }
+}
